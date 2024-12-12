05e0e01d304fbdbab442350e529e3ebaad0a3de3
RicardoYam <10678784+ricardoyam@user.noreply.gitee.com>
2024-12-12 17:36:45 +1000

    Update crypto.ts

--- src/pages/api/crypto.ts
+++ src/pages/api/crypto.ts
@@ -1,5 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default async function handler(
   req: NextApiRequest,
